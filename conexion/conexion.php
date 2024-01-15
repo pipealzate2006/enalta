0b3def23a1797e7bf6b60491429bb8a244811505
pipealzate2006 <141877758+pipealzate2006@users.noreply.github.com>
2024-01-15 17:21:01 -0600

    Update conexion.php

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "id21795689_enaltashop30");
+$conexion = new mysqli("localhost", "root", "", "enalta1.0");
 if ($conexion->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 } 
